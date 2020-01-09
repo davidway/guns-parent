3dc50960d70ebf69a79b705f987491c94f18baae
朱伟强 <zhuweiqiang@mail.weisanyun.com>
2020-01-09 18:10:52 +0800

    Initial commit

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/impl/UserAPIImpl.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.api.impl;
+
+import com.stylefeng.guns.api.UserAPI;
+
+public class UserAPIImpl implements UserAPI {
+    @Override
+    public boolean login(String username, String password) {
+        return false;
+    }
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserAPIImpl.java
@@ -0,0 +1,11 @@
+package com.stylefeng.guns.rest.modular.user;
+
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+@Service(interfaceClass = UserAPI.class)
+public class UserAPIImpl {
+}
